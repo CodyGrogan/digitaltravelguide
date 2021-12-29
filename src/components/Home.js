919c57f046c36757d19967fff9cc1f2b7e8377bf
Cody Grogan <cod.grogan@gmail.com>
2021-12-29 15:31:40 +0800

    adjusted home style

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home(props){
         <div className="pageCon">
             <h1>
                 Welcome To Digital Travel Guide Taipei
-            </h1>
+            </h1> <div className="lineBreak"></div>
             <Link to='/quiz' ><button className="btn btn-primary">Plan your vacation!</button></Link>
             
             
