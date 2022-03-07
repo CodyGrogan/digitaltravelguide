c43c4768a9995ad45cf7f3e7657d7a66c2a77761
Cody Grogan <cod.grogan@gmail.com>
2022-03-07 19:49:18 +0800

    reduced number of quiz questions

--- src/components/questionList.js
+++ src/components/questionList.js
@@ -12,17 +12,17 @@ let gallery = new Question('art', 'art galleries');
 let spicy = new Question('spicy', 'spicy food')
 let sushi = new Question('japanese', 'sushi');
 let outdoor = new Question('nature', 'the great outdoors');
-let museum = new Question('history', 'history museums');
+let museum = new Question('history', 'historical sites and museums');
 let tall = new Question('sight seeing', 'tall buildings');
 let noodles = new Question('japanese', 'noodles');
 let sites = new Question('history', 'historic sites');
-let garden = new Question('nature', 'gardens');
+let garden = new Question('nature', 'gardens and parks');
 let artsandcrafts = new Question('art', 'arts and crafts');
 let nightlife = new Question('nightlife', 'night life');
 let shopping = new Question('shopping', 'shopping');
 let hiking = new Question('hiking', 'hiking');
 
-let questionObjArr = [dumpling, museum, chinese, spicy, gallery, sushi, outdoor, tall, noodles, sites, garden, artsandcrafts, nightlife, shopping, hiking];
+let questionObjArr = [chinese, museum, spicy, gallery, sushi, tall, garden, artsandcrafts, nightlife, shopping, hiking];
 
 
 export default questionObjArr;
